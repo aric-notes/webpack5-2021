20eb037b4bcae623c5c1e2bc81185269ae0ae227
feizheng <1290657123@qq.com>
2022-01-08 21:10:18 +0800

    feat: chap 06

---
+++ packages/chapter-06/src/main.js
@@ -0,0 +1,7 @@
+import {sum, square} from "./utils";
+import './main.css';
+
+console.log(
+    sum(1, 2),
+    square(4)
+)
---
+++ packages/chapter-06/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.css$/,  // 正则，匹配将要处理的文件(filename)类型
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
